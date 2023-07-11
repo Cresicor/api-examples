9cf8c566852cff0c6b315db06178932c8c3690ce
Daniel Whatley <dwhatley@mit.edu>
2023-07-11 14:51:18 -0700

    python example

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# api-examples
-Vividly API Examples
+# Vividly API Examples
+
+Vividly's GraphQL API exports data from the Vividly platform in a format customizable to your needs.
+
+## Requirements
+
+You will need your Vividly _company ID_ as well as an _API token_. These can both be obtained from the `Settings` section of Vividly.
+
+## Use of the Vividly API
+
+Currently the Vividly API is in a _beta_ phase. It is available at this URL: `https://pr-2004.vividly.dev/public/graphql/`
+
+You will need to pass two headers to each API call
+- an API bearer token as the `Authorization` header
+- your company ID as the `Company` header
+
+## Examples
+
+Please see the `examples/` folder for some examples of the API and its use.
---
+++ examples/python-example.py
@@ -0,0 +1,42 @@
+# This is an example of how to call Vividly's API from a Python script.
+# You will need to use your own company ID and API token.
+
+import requests
+
+# IMPORTANT: We highly recommend not storing these values in plaintext on the web!
+# Please use a secret manager or some other method of encrypted storage.
+VIVIDLY_API_TOKEN = "TODO"
+VIVIDLY_COMPANY_ID = "TODO"
+
+# The query you wish to perform.
+# This retrieves a subset of information from all the promotions in your company
+# whose names contain "KeHE".
+GRAPHQL_QUERY = """
+query {
+    promotions (filters: { name: { contains: "KeHE" } }) {
+        name
+        isStarred
+        promotionlinePromotion {
+            buyinEndDate
+            buyinStartDate
+            scanbackEndDate
+            scanbackStartDate
+        }
+    }
+}
+"""
+
+# Call the Vividly API.
+response = requests.post(
+    "https://pr-2004.vividly.dev/public/graphql/",
+    json={"query": GRAPHQL_QUERY},
+    headers={
+        "Authorization": f"Token {VIVIDLY_API_TOKEN}",
+        "Company": VIVIDLY_COMPANY_ID,
+    }
+)
+
+# Here, we print the response.
+# You (the user) can choose what to do with this response.
+print(f"Response code {response.status_code}")
+print(f"Body:\n{response.text}")
